3289f49354c7413ee45715b2b7dc0e55f42343f4
Sorrelljs <[sjsohota@gmail.com]>
2020-02-26 18:31:37 -0800

    making templates

---
+++ themes/karakata/.page-contact.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This is the contact for our theme.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
---
+++ themes/karakata/.page-events.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The events page for our theme.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
