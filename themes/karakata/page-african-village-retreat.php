2513a4e756ffb26c6ce2324d8f74f1ce535cf210
Sorrelljs <[sjsohota@gmail.com]>
2020-02-27 18:04:27 -0800

    added menu and correct pages

--- themes/karakata/page-african-village-retreat.php
+++ themes/karakata/page-african-village-retreat.php
@@ -1,1 +1,8 @@
-<h1>test</h1>
+<?php get_header() ?>
+
+<?php get_the_title() ?>
+
+<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu' => 'African Retreat' ) ); ?>
+
+
+<?php get_footer() ?>
