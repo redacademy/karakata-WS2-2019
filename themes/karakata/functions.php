5f74ee224122c23b3ea3fbaa12d5c59420dc82c2
Sorrelljs <[sjsohota@gmail.com]>
2020-03-02 20:49:45 -0800

    classes are updated

--- themes/karakata/functions.php
+++ themes/karakata/functions.php
@@ -89,6 +89,7 @@ function red_starter_scripts() {
 	wp_enqueue_script( 'flickityjs', get_template_directory_uri() . '/build/js/flickity.pkgd.min.js', array('jQuery'), '20151215', true );
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'functions', get_template_directory_uri() . '/build/js/functions.min.js', array ( 'jquery' ), '', true);
+	wp_enqueue_script( 'classes-navigation', get_template_directory_uri() . '/build/js/classes-navigation.min.js', array ( 'jquery' ), '', true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ themes/karakata/js/classes-navigation.js
@@ -0,0 +1,15 @@
+
+(function ($) {
+
+
+    $('.service-icon').click(function () {
+        $(this).addClass('active');
+        $('.service-icon').not('.active').hide();
+        const selected = $(this).data('section');
+        $('.service-info-box[data-section="' + selected + '"]').show();
+    });
+
+
+
+
+})(jQuery);
