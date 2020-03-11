48879a76c258ed79aaed85bbc638323bb929a45f
smeggins <phillipchadwick@hotmail.com>
2020-03-10 19:05:59 -0700

    comments out the offending code in functions.php to be resolved later. comments out offending code in ajax.js to be resolved later. replaced a taxonomy

--- plugins/karakara-functionality/lib/functions/taxonomies.php
+++ plugins/karakara-functionality/lib/functions/taxonomies.php
@@ -5,8 +5,7 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_taxonomy
  */
 
-// Add your custom taxonomies here...
-
+// Register Custom Taxonomy
 function service_taxonomy() {
 
 	$labels = array(
