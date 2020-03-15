6d07c51e6ce5c8c1a00ab5fb815b2ee7f0c7d769
ermia <afsharianarmia@gmail.com>
2020-03-14 17:29:15 -0700

    fix new event post type not showing up on event page & a bug in the event content code

--- themes/karakata/template-parts/content-events.php
+++ themes/karakata/template-parts/content-events.php
@@ -7,34 +7,46 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-content">
-	<?php $lastposts = get_posts(
-            array(
-                'posts_per_page' => 3,
-                'post_type' => 'events_post_type',
-                 'category_name' => 'event'
-	)); 
-	if ( $lastposts ) {
-		echo '<div class="events-container">';
-		foreach ( $lastposts as $post ) :
-			setup_postdata( $post ); ?>
-			<div class="event">
-			<div class="event-image"><?php echo get_the_post_thumbnail(); ?></div>
-			<div class="event-info">
-			<div class="date"><?php echo get_field('event_date');?></div>
-			<div class="price"><?php echo get_field('price');?></div>
-			<h2 class='event-title'><?php the_title();?></h2>
-			<p class='event-address'><i class="fas fa-map-marker-alt"></i><?php echo get_field('event_address');?></p>
-			<a href="../contact/" class='find-out'>Find out</a>
-			</div></div>
-		</div>
-		<?php
-		endforeach; 
-		echo '</div>';
-		wp_reset_postdata();
-	}
-	?>
+	<div class="entry-event">
+	<?php $args =  array(
+            'post_type' => 'event_post_type',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'show',
+                    'field'    => 'slug',
+                    'terms'    => 'event',
+                    
+                ),
+            ),
+        ); 
+        ?>
+            <?php
+
+            $the_query = new WP_Query( $args );
+            
+            if ( $the_query->have_posts() ) {
+            
+                while ( $the_query->have_posts() ) {
+                    $the_query->the_post();
+                    $featured = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+					?>
+					<div class="event">
+						<div class="event-image"><?php echo get_the_post_thumbnail(); ?></div>
+						<div class="event-info">
+							<div class="date"><?php echo get_field('event_date');?></div>
+							<div class="price"><?php echo get_field('price');?></div>
+							<h2 class='event-title'><?php the_title();?></h2>
+							<p class='event-address'><i class="fas fa-map-marker-alt"></i><?php echo get_field('event_address');?></p>
+							<a href="../contact/" class='find-out'>Find out</a>
+						</div></div>
+					<?php
+                }
+            } 
+            else {
+                // no posts found
+            }
+            wp_reset_postdata();
+            
+            ?>
 	</div><!-- .entry-content -->
-</article><!-- #post-## -->
