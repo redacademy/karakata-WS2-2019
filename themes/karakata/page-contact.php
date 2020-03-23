7585ef9b170c50ce72a173630bc54528afc33aaa
ermia <afsharianarmia@gmail.com>
2020-03-22 22:14:19 -0700

    fix events page bug

--- themes/karakata/page-contact.php
+++ themes/karakata/page-contact.php
@@ -134,7 +134,7 @@ if(isset($_POST['email'])) {
  </div>
   </div>
             <div class="contact-ad">
-            <h1>come with us.</h1>
+            <h1>come visit us.</h1>
             <iframe class="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2610.3065162993516!2d-122.31389488431577!3d49.13780447931589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548433f95a74b659%3A0x10f4057ff8cf5483!2s7487%20Birch%20St%2C%20Mission%2C%20BC%20V2V%203P9!5e0!3m2!1sen!2sca!4v1583891038771!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             <p class="contact-address"><i class="fas fa-map-marker-alt"></i>7487 brich st mission , british columbia</p>
             </div>
