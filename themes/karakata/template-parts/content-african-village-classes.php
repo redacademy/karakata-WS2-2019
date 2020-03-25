410226f38d8862e41411a5e81dc77ff1d5a2a1a2
Sorrelljs <[sjsohota@gmail.com]>
2020-03-24 19:33:03 -0700

    updating desktop styling

--- themes/karakata/template-parts/content-african-village-classes.php
+++ themes/karakata/template-parts/content-african-village-classes.php
@@ -15,35 +15,46 @@
             <div class="about-content">
                 <div class = "services-provided">
 
-
-                <a class = "music-service service-icon" data-section="music">      
-                <img src="<?php echo get_template_directory_uri(); ?>/Icons/Music_White.svg" scale="0">
-                    <p class = "section-tag">Music</p>    
+        
+                <a class = "music-service service-icon" data-section="music">   
+                    <div class ="class_icon_container">
+                        <img class = "class_icon"src="<?php echo get_template_directory_uri(); ?>/Icons/Music_White.svg" scale="0">
+                            <p class = "section-tag">Music</p>   
+                    </div> 
                 </a>
                     
 
                 <a class = "drums-service service-icon" data-section="drums">
-                    <img src="<?php echo get_template_directory_uri(); ?>/Icons/Drums_White.svg" scale="0">
-                        <p class = "section-tag">Drums</p>
+                    <div class ="class_icon_container">
+                        <img class = "class_icon"src="<?php echo get_template_directory_uri(); ?>/Icons/Drums_White.svg" scale="0">
+                            <p class = "section-tag">Drums</p>
+                    </div>
                 </a>
 
                 <a class = "yoga-service service-icon" data-section="yoga">
-                    <img src="<?php echo get_template_directory_uri(); ?>/Icons/Yoga_White.svg" scale="0">
-                        <p class = "section-tag">Yoga</p>
+                    <div class ="class_icon_container">
+                        <img class = "class_icon"src="<?php echo get_template_directory_uri(); ?>/Icons/Yoga_White.svg" scale="0">
+                            <p class = "section-tag">Yoga</p>
+                    </div>
                 </a>
 
                 <a class = "meditation-service service-icon" data-section="meditation"> 
-                    <img src="<?php echo get_template_directory_uri(); ?>/Icons/Meditation_White.svg" scale="0">
-                        <p class = "section-tag">Meditation</p>
+                     <div class ="class_icon_container">
+                          <img class = "class_icon"src="<?php echo get_template_directory_uri(); ?>/Icons/Meditation_White.svg" scale="0">
+                            <p class = "section-tag">Meditation</p>
+                    </div>
                 </a>
                 <a class = "cook-service service-icon" data-section="cook">
-                    <img src="<?php echo get_template_directory_uri(); ?>/Icons/Cook_White.svg" scale="0">
+                  <div class ="class_icon_container">
+                    <img class = "class_icon"src="<?php echo get_template_directory_uri(); ?>/Icons/Cook_White.svg" scale="0">
                         <p class = "section-tag">Cook</p>
+                </div>
                 </a>
                 <a class = "contact-service service-icon" data-section="contact">
-                    <img src="<?php echo get_template_directory_uri(); ?>/Icons/Contact_White.svg" scale="0">
-                        <p class = "section-tag">Contact</p>
-
+                     <div class ="class_icon_container">
+                        <img class = "class_icon"src="<?php echo get_template_directory_uri(); ?>/Icons/Contact_White.svg" scale="0">
+                            <p class = "section-tag">Contact</p>
+                    </div>
                 </a>
 
             <a class = "back-button">
