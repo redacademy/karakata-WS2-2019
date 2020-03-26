d539cf08665c834a2ce176197c3703f5483818ad
Sorrelljs <[sjsohota@gmail.com]>
2020-03-26 15:30:14 -0700

    fixed some minor bugs, doing cross browser checking

--- themes/karakata/js/classes-navigation.js
+++ themes/karakata/js/classes-navigation.js
@@ -19,25 +19,26 @@
     $('.service-icon').removeClass('triggered-c');
     $('.service-info-box').addClass('hidden');
     $('.class-card-container').html('');
-    $('.about-content').removeClass('trigger-sp1')
-    $('.services-provided').removeClass('trigger-sp2')
+    $('.about-content').removeClass('trigger-sp1');
+    $('.services-provided').removeClass('trigger-sp2');
   });
 
-  let classesPull = function (val, card) {
+  let classesPull = function(val, card) {
     // used to generate the classes pages ticket content based in which category is selected
-    if($(card).hasClass('triggered-c') === false) {
-      $(card).addClass('triggered-c')
-      $('.about-content').addClass('trigger-sp1')
-      $('.services-provided').addClass('trigger-sp2')
+    if ($(card).hasClass('triggered-c') === false) {
+      $(card).addClass('triggered-c');
+      $('.about-content').addClass('trigger-sp1');
+      $('.services-provided').addClass('trigger-sp2');
 
       $.ajax({
-          method: 'GET',
-          url: functionVars.karakata_url + 'acf/v3/services_post_type' ,
-          async: false,
-          success: function (data) {
-            data.forEach((song) => {
-              if(song.acf.category === val) {
-                $('.class-card-container').append(`<div class="service-info-box music-test" data-section="music">\
+        method: 'GET',
+        url: functionVars.karakata_url + 'acf/v3/services_post_type',
+        async: false,
+        success: function(data) {
+          data.forEach(song => {
+            if (song.acf.category === val) {
+              $('.class-card-container')
+                .append(`<div class="service-info-box music-test" data-section="music">\
                 <div class = "event-information-wrap">\
                     <p class = "event-title-tab">${song.acf.title_upper}</p>\
                     <div class = "event-address-tab"><div class = "class-address"><img src = "${functionVars.karakata_template_path}/Icons/Location.png"></div><p>${song.acf.address}</p></div>\
@@ -49,46 +50,76 @@
                     </div>\
                 </div>\
             </div>`);
-              }'/Icons/Location.png'
-            })  
-          },
-          complete: function() {
-          },
+            }
+            ('/Icons/Location.png');
+          });
+        },
+        complete: function() {},
 
-          beforeSend: function(xhr) {
-              xhr.setRequestHeader('X-WP-Nonce', functionVars.karakata_nonce);
-          }
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', functionVars.karakata_nonce);
+        }
       });
     }
   };
 
   $('.music-service').on('click', function() {
     $('.music-test').removeClass('hidden');
-    classesPull($(this).find($('.section-tag')).html(), this)
+    classesPull(
+      $(this)
+        .find($('.section-tag'))
+        .html(),
+      this
+    );
   });
 
   $('.drums-service').on('click', function() {
     $('.drums-test').removeClass('hidden');
-    classesPull($(this).find($('.section-tag')).html(), this)
+    classesPull(
+      $(this)
+        .find($('.section-tag'))
+        .html(),
+      this
+    );
   });
 
   $('.yoga-service').on('click', function() {
     $('.yoga-test').removeClass('hidden');
-    classesPull($(this).find($('.section-tag')).html(), this)
+    classesPull(
+      $(this)
+        .find($('.section-tag'))
+        .html(),
+      this
+    );
   });
   $('.meditation-service').on('click', function() {
     $('.meditation-test').removeClass('hidden');
-    classesPull($(this).find($('.section-tag')).html(), this)
+    classesPull(
+      $(this)
+        .find($('.section-tag'))
+        .html(),
+      this
+    );
   });
 
   $('.cook-service').on('click', function() {
     $('.cook-test').removeClass('hidden');
-    classesPull($(this).find($('.section-tag')).html(), this)
+    classesPull(
+      $(this)
+        .find($('.section-tag'))
+        .html(),
+      this
+    );
   });
 
   $('.contact-service').on('click', function() {
     $('.contact-test').removeClass('hidden');
-    classesPull($(this).find($('.section-tag')).html(), this)
+    classesPull(
+      $(this)
+        .find($('.section-tag'))
+        .html(),
+      this
+    );
   });
 
   // come back to this to make the correct box infomation to go with correct tab
